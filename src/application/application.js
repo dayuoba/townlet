f3d33db19f2bd8c95c09fd52da52724ae833b765
dayuoba <taddayu@sina.com>
2015-03-29 12:33:27 +0800

    implement some api in application.js

--- src/application/application.js
+++ src/application/application.js
@@ -3,30 +3,39 @@
 //Contributors @dayuoba @kingzez
 //Description:Application apis
 
+var appUtil = require('./appUtil');
+
 //application constructor
-var Application = module.exports = function() {
+var Application = module.exports = function(config) {
 	this.settings = {};
 	this.STATUS = 0;
 	this.components = {};
 	this.utils = {};
+	this.config = config || {};
+};
+//[Description] 
+Application.prototype.set = function(key, value, attach) {
+	if (attach) this[key] = value;
+	this.settings[key] = value;
+	return this;
 };
 
-Application.prototype.set = function(first_argument) {
-	// body...
+Application.prototype.get = function(key) {
+	return this.settings[key];
 };
-
-Application.prototype.get = function(first_argument) {
-	// body...
+//[Description] Load with file path eg:load('util', './appUtil'); //this.util = require('./appUtil');
+//[Suggestion] considering of how to load our components to make them can be loaded by order
+//                      and can be consturcted using application
+Application.prototype.load = function(key, path) {
+	//TODO
+	return this;
 };
-
-Application.prototype.load = function(first_argument) {
-	// body...
-};
-
+//[Description] Load utils,components,init components
 Application.prototype.init = function(first_argument) {
 	// body...
 };
-
+//[Description] run application -> run components by order
+//[Suggestion] considering of how to make the components run life-cycle correctly
 Application.prototype.run = function(first_argument) {
 	// body...
 };
